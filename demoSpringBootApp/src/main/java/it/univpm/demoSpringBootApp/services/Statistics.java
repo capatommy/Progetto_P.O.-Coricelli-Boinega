5789c734e57c2732d597ed367b008612b1566e5c
BOINE98 <57906329+BOINE98@users.noreply.github.com>
2019-12-06 16:01:31 +0100

    Update Statistics.java
    aggiustamento vario

--- demoSpringBootApp/src/main/java/it/univpm/demoSpringBootApp/services/Statistics.java
+++ demoSpringBootApp/src/main/java/it/univpm/demoSpringBootApp/services/Statistics.java
@@ -10,14 +10,14 @@ public abstract class Statistics {
 	
 	/*metodo per calcolare la media*/
 	
-	public static double avg(List<Double> lista) {
-        return sum(lista) / count(lista);
+	public static double media(List<Double> lista) {
+        return somma(lista) / numeroelementi(lista);
     }
 	
 	
 	/*metodo per individuare il minimo della lista */
 	
-	public static double min(List<Double> lista) {
+	public static double minimo(List<Double> lista) {
 		double minimo = lista.get(0);
 		for( Double numero : lista) {
 			if (numero<minimo)  minimo = numero;
@@ -28,75 +28,78 @@ public abstract class Statistics {
 	
 	/*metodo per individuare il massimo della lista*/
 	
-	public static double max(List<Double> lista) {
+	public static double massimo(List<Double> lista) {
 		double massimo = lista.get(0);
 		for (Double numero: lista) {
 			if (numero>massimo) massimo= numero;
 		}
 		return massimo;
 	}
-	
-	
-	/*metodo per il calcolo dello scarto quadratico medio*/
-	public static double devstd(List<Double> lista) {
-		double avg = avg(lista);
-		double varianza = 0;
-		for (Double n: lista) {
-			varianza += Math.pow(n-avg, 2);
-			}
-		return Math.sqrt(varianza);
-	}
+
 	
 	
 	/*metodo per sommare ogni elemento  */
-	public static double sum(List <Double> lista) {
-		double s = 0;
+	public static double somma(List <Double> lista) {
+		double somma = 0;
 		for (Double valore: lista) {
-			s += valore;
+			somma += valore;
 		}
-		return s;
+		return somma;
 	}
 	
 	/*metodo per contare gli lementi della lista*/
-	public static int count (List lista) {
+	public static int numeroelementi (List lista) {
 		return lista.size();
 	}
 	
+	
 	/*metodo per contare le occorrenze di ogni elemento della lista*/
 	public static Map<Object, Integer> conteggioElementiUnici(List lista){
-		Map<Object, Integer> member = new HashMap<>();
-		for (Object o: lista) {
-			Integer numero = member.get(o);
-			member.put(o,  (numero== null ? 1:numero+1));
+		Map<Object, Integer> membro = new HashMap<>();
+		for (Object v: lista) {
+			Integer occorrenza = membro.get(v);
+			if (occorrenza == null) {occorrenza= 1;
+			membro.put(v,  occorrenza);}
+			else {occorrenza = new Integer(occorrenza.intValue() + 1);
+			membro.put(v, occorrenza);
+			}
 		}
-		return member;
+		return membro;
 		
+	}
+	
+	/*metodo per il calcolo dello scarto quadratico medio*/
+	public static double deviazionestandard(List<Double> lista) {
+		double vm =media(lista);
+		double varianza = 0;
+		double risultato;
+		for (Double n: lista) {
+			varianza += Math.pow(n-vm, 2);
+			}
+		risultato = Math.sqrt(varianza);
+		return risultato;
 	}
 	
 	/*metodo per ottenere tutti i valori de imetodi precedenti una volta scelto il campo*/
 	public static Map<String, Object> getTutteLeStatistiche(String NomeDelCampo, List lista){
-		Map<String, Object> map = new HashMap<>();
-		map.put("campo", "nomeCampo");
-		if (!lista.isEmpty()) { //se la lista è piena allora... riempi la mappa con le statistiche in base l'anno
+		Map<String, Object> mappa = new HashMap<>();
+		mappa.put("campo", "nomeCampo");
+ // riempimento della mappa con le statistiche in base l'anno
 			if (TSVReader.anni.contains(NomeDelCampo)) {
 				List <Double> listavalori = new ArrayList<>();
-				for (Object o : lista) {
-					listavalori.add( ((Double) o) );	
+				for (Object v : lista) {
+					listavalori.add( ((Double) v) );	
 				}
 				
-				map.put("avg", avg(listavalori));
-				map.put("min", min(listavalori));
-				map.put("max", max(listavalori));
-				map.put("devstd", devstd(listavalori));
-				map.put("sum", sum(listavalori));
-				map.put("count", count(listavalori));
-				return map;
+				mappa.put("somma", media(listavalori));
+				mappa.put("minimo", minimo(listavalori));
+				mappa.put("massimo", massimo(listavalori));
+				mappa.put("deviazionestandard", deviazionestandard(listavalori));
+				mappa.put("somma", somma(listavalori));
+				mappa.put("numeroelementi", numeroelementi(listavalori));
+				mappa.put("elementiunici",conteggioElementiUnici(lista));
+			    }
+			
+			return mappa;
 			}
-			else {map.put("elementiunici",conteggioElementiUnici(lista)); 
-			map.put("count", count(lista));
-			}
-		}
-		return map;
-	}
-	
-}	
+}
